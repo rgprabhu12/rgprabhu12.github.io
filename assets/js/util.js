9933cf8641b1529ab44f9bcb0ee82f5508f9f8b9
Rohan Prabhu <rgprabhu@ncsu.edu>
2017-08-01 21:48:33 -0400

    so much stuff

--- assets/js/util.js
+++ assets/js/util.js
@@ -34,6 +34,88 @@
 
 	};
 
+	$(window).load(function(){
+			var pages = $('#container li'), current=0;
+			var currentPage,nextPage;
+			var timeoutID;
+			var buttonClicked=0;
+
+			var handler1=function(){
+				buttonClicked=1;
+				$('#container .button').unbind('click');
+				currentPage= pages.eq(current);
+				if($(this).hasClass('prevButton'))
+				{
+					if (current <= 0)
+						current=pages.length-1;
+					else
+						current=current-1;
+					nextPage = pages.eq(current);
+
+					nextPage.css("marginLeft",-604);
+					nextPage.show();
+					nextPage.animate({ marginLeft: 0 }, 800,function(){
+						currentPage.hide();
+					});
+					currentPage.animate({ marginLeft: 604 }, 800,function(){
+						$('#container .button').bind('click',handler1);
+					});
+				}
+				else
+				{
+
+					if (current >= pages.length-1)
+						current=0;
+					else
+						current=current+1;
+					nextPage = pages.eq(current);
+
+					nextPage.css("marginLeft",604);
+					nextPage.show();
+					nextPage.animate({ marginLeft: 0 }, 800,function(){
+					});
+					currentPage.animate({ marginLeft: -604 }, 800,function(){
+						currentPage.hide();
+						$('#container .button').bind('click',handler1);
+					});
+				}
+			}
+
+			var handler2=function(){
+				if (buttonClicked==0)
+				{
+				$('#container .button').unbind('click');
+				currentPage= pages.eq(current);
+				if (current >= pages.length-1)
+					current=0;
+				else
+					current=current+1;
+				nextPage = pages.eq(current);
+				nextPage.css("marginLeft",604);
+				nextPage.show();
+				nextPage.animate({ marginLeft: 0 }, 800,function(){
+				});
+				currentPage.animate({ marginLeft: -604 }, 800,function(){
+					currentPage.hide();
+					$('#container .button').bind('click',handler1);
+				});
+				timeoutID=setTimeout(function(){
+					handler2();
+				}, 4000);
+				}
+			}
+
+			$('#container .button').click(function(){
+				clearTimeout(timeoutID);
+				handler1();
+			});
+
+			timeoutID=setTimeout(function(){
+				handler2();
+				}, 4000);
+
+	});
+
 	/**
 	 * Panel-ify an element.
 	 * @param {object} userConfig User config.
